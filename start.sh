f8ed40794486db1d5d48d01ead1334eccaca8334
Victor ballu <victor.ballu@clever-cloud.com>
2019-06-27 17:00:05 +0200

    fsbucket

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-export JUPYTER_CONFIG_DIR=$APP_HOME
 
-jupyter notebook --no-browser --port 8080 --ip=0.0.0.0
+jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --notebook-dir=$APP_HOME/notebook
