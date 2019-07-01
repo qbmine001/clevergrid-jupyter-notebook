74c902c409a180f1a3b204616a7bbd31982f75a7
Victor ballu <victor.ballu@clever-cloud.com>
2019-07-01 13:17:12 +0200

    typo

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkidr workdir
+mkdir workdir
 
 jupyter serverextension enable --py bookstore
 jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --config=jupyter_notebook_config.py --notebook-dir=$APP_HOME/workdir
