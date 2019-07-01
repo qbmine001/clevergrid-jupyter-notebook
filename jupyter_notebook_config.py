cbfb7831f6380f52d982adf4377c12fc4a2f2f5e
Victor ballu <victor.ballu@clever-cloud.com>
2019-07-01 16:04:00 +0200

    fix env var management

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,14 +1,19 @@
-
+import os
 from s3contents import S3ContentsManager
+
+CELLAR_ADDON_KEY_ID = os.getenv("CELLAR_ADDON_KEY_ID")
+CELLAR_ADDON_KEY_SECRET = os.getenv("CELLAR_ADDON_KEY_SECRET")
+BUCKET_NOTEBOOK_NAME = os.getenv("BUCKET_NOTEBOOK_NAME")
+CELLAR_ADDON_HOST = os.getenv("$CELLAR_ADDON_HOST")
 
 c = get_config()
 
 # Tell Jupyter to use S3ContentsManager for all storage.
 c.NotebookApp.contents_manager_class = S3ContentsManager
-c.S3ContentsManager.access_key_id = $CELLAR_ADDON_KEY_ID
-c.S3ContentsManager.secret_access_key = $CELLAR_ADDON_KEY_SECRET
-c.S3ContentsManager.bucket = $BUCKET_NOTEBOOK_NAME
-c.S3ContentsManager.endpoint_url = "https://$CELLAR_ADDON_HOST"
+c.S3ContentsManager.access_key_id = CELLAR_ADDON_KEY_ID
+c.S3ContentsManager.secret_access_key = CELLAR_ADDON_KEY_SECRET
+c.S3ContentsManager.bucket = BUCKET_NOTEBOOK_NAME
+c.S3ContentsManager.endpoint_url = "https://" + CELLAR_ADDON_HOST
 
 # Optional settings:
 c.S3ContentsManager.prefix = ""
