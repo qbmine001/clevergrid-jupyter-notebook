27d1ec93a3bf70afff3a49d20758f5660c4b3201
Victor ballu <victor.ballu@clever-cloud.com>
2019-07-01 13:09:16 +0200

    update working dir

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 jupyter serverextension enable --py bookstore
-jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --config=jupyter_notebook_config.py
+jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --config=jupyter_notebook_config.py --notebook-dir=$APP_HOME/workdir
