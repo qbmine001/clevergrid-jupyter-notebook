5112b3046dae91a4fbfc71c24821f41992b39210
Victor ballu <victor.ballu@clever-cloud.com>
2019-07-01 17:23:47 +0200

    fix typo bug

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python ./bucket-management/bucket_management.py $CELLAR_ADDON_HOST $CELLAR_ADDON_KEY_ID $CELLAR_ADDON_KEY_SECRET --create-bucket BUCKET_NOTEBOOK_NAME --ignore
+python ./bucket-management/bucket_management.py $CELLAR_ADDON_HOST $CELLAR_ADDON_KEY_ID $CELLAR_ADDON_KEY_SECRET --create-bucket $BUCKET_NOTEBOOK_NAME --ignore
 
 jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --config=jupyter_notebook_config.py
