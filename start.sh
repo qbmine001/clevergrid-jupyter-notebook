07ab945ea3c5e925d38ae5396a9a8eedc9345506
vballu <victor.ballu@clever-cloud.com>
2019-07-02 11:08:22 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Create a storage bucket if it not exists
 python ./bucket-management/bucket_management.py $CELLAR_ADDON_HOST $CELLAR_ADDON_KEY_ID $CELLAR_ADDON_KEY_SECRET --create-bucket $BUCKET_NOTEBOOK_NAME --ignore
 
+# Run jupyter on the port 8080, granted remote access and set a specific jupyter configuration file
 jupyter notebook --no-browser --port 8080 --ip=0.0.0.0 --config=jupyter_notebook_config.py
